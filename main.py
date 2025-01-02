0d3341303630cdb13e50713af60ffd9736fed836
astropsy999 <bareust@gmail.com>
2025-01-02 08:51:10 +0300

    network init

---
+++ main.py
@@ -0,0 +1,14 @@
+class NeuralNetwork:
+    def __init__(self, input_nodes, hidden_nodes, output_nodes, learning_rate):
+        self.input_nodes = input_nodes
+        self.hidden_nodes = hidden_nodes
+        self.output_nodes = output_nodes
+        self.learning_rate = learning_rate
+
+        pass
+
+    def train(self, inputs_list, targets_list):
+        pass
+
+    def query(self, inputs_list):
+        pass
